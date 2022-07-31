d910ed0a9d706524c360996c13ad11d2e3abdacf
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2022-07-31 11:37:22 -0400

    wait what was I doing?

--- Assets/Scripts/Managers/GameplayManager.cs
+++ Assets/Scripts/Managers/GameplayManager.cs
@@ -5,15 +5,43 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Update()
     {
-        
+        //if chamber is active,
+        //MonitorChamber();
+        //else, do nothing (that I can think of...)
     }
 
-    // Update is called once per frame
-    void Update()
+    public void sessionStart(int floor, int chamber)
     {
-        
+        //if floor, chamber == 0, start new game and load first floor
+        //else, load specified floor.
+        //LoadFloor(floor); //Loads current floor scene.
+
+        //LoadCharacter(); //Will create blank character if no character save exists
+        //GenerateChamber(); //Generates chamber based on current floor and chamber. 
+        //ChamberStart(); //Enables gameplay in the current chamber.
+    }
+
+    public void ChamberStart()
+    {
+        //Places character in chamber via animation, then starts combat.
+    }
+
+    public void MonitorChamber()
+    {
+        //Monitor player progress of objective, updating UI via UI manager as needed.
+        //When Objective completed, open exit.
+        //On entering exit trigger:
+        //ChamberEnd();
+    }
+
+    public void ChamberEnd()
+    {
+        //Removes player from room, checks if last chamber finished. If so,
+        //LoadFloor(floor + 1);
+        //if not, generates new chamber, and cycles to it via animation.
+        //GenerateChamber()
+        //ChamberStart()
     }
 }
---
+++ Assets/Scripts/ProtoEnemyBehavior.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoEnemyBehavior : MonoBehaviour
+{
+
+    private GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
