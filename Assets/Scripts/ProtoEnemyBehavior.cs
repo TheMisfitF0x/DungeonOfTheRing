be07f9c20dad80fa9a9dccaad3a7fbfd6096dff2
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2023-05-05 11:45:08 -0400

    intro state machine code
    For AI behavior

--- Assets/Scripts/ProtoEnemyBehavior.cs
+++ Assets/Scripts/ProtoEnemyBehavior.cs
@@ -6,9 +6,14 @@ public class ProtoEnemyBehavior : MonoBehaviour
 {
 
     private GameObject player;
+
+    private List<string> states = new List<string>();
+    private string currentState;
+
+
     void Start()
     {
-        
+        states.AddRange(new string[] { "Moving", "Aiming", "Shooting" });
     }
 
     // Update is called once per frame
