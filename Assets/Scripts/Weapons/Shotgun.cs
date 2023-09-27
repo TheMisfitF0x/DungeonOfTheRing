969a13b6bc45ff8c2f61a3713e6306a714f54dd4
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2023-09-27 08:34:06 -0400

    fix Shotgun spread

--- Assets/Scripts/Weapons/Shotgun.cs
+++ Assets/Scripts/Weapons/Shotgun.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Shotgun : Weapon
 {
     //Rounds fired per valid trigger pull
-    public int projectilesPerShot = 1;
+    public int projectileCount = 3;
 
     //True if rounds are to be evenly spaced within the spread
     public bool uniformDistroAngle = false;
@@ -16,22 +16,17 @@ public class Shotgun : Weapon
     {
         if (Time.time >= lastShotTime + fireDelay)
         {
-            for (int i = -1; i < projectilesPerShot -1; i++)
+            for (int i = 0; i < projectileCount; i++)
             {
-                Vector3 exitAngle = new Vector3(i,0,0);
-                exitAngle = muzzle.InverseTransformVector(exitAngle);
-                if (uniformDistroAngle)
-                {
-                    
-                }
-                else
-                {
+                // Calculate the spread angle for each pellet
+                float angle = transform.eulerAngles.z - spread / 2 + Random.Range(0f, spread);
 
-                }
-                GameObject bullet = Instantiate(projectile, muzzle.position, muzzle.rotation);
-                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-                Debug.Log(muzzle.up + exitAngle);
-                rb.AddForce((muzzle.up + exitAngle) * muzzleVelocity, ForceMode2D.Impulse);
+                // Create a new pellet instance
+                GameObject pellet = Instantiate(projectile, transform.position, Quaternion.Euler(0f, 0f, angle));
+
+                // Apply force to the pellet
+                Rigidbody2D rb = pellet.GetComponent<Rigidbody2D>();
+                rb.velocity = pellet.transform.up * muzzleVelocity;
             }
 
             lastShotTime = Time.time;
